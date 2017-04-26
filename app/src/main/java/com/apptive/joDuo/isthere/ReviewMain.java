1e4c7e12e1a81738aebe5b5dfc68c0c0d839cfab
extJo <josungyun6518@gmail.com>
2017-04-26 17:36:47 +0900

    중간보고 발표

--- app/src/main/java/com/apptive/joDuo/isthere/ReviewMain.java
+++ app/src/main/java/com/apptive/joDuo/isthere/ReviewMain.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RelativeLayout;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 
@@ -18,9 +20,24 @@ public class ReviewMain extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.review_main);
 
+//    MapPoint pnu = MapPoint.mapPointWithGeoCoord(35.23, 129.07);
+
 
     MapView mapView = new MapView(this);
     mapView.setDaumMapApiKey(getString(R.string.daum_map_key));
+
+    // 커스텀 마커 추가
+    MapPOIItem customMarker = new MapPOIItem();
+    customMarker.setItemName("Custom Marker");
+    customMarker.setTag(1);
+//    customMarker.setMapPoint(pnu);
+    customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+    customMarker.setCustomImageResourceId(R.drawable.ic_pin); // 마커 이미지.
+    customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+    customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+    mapView.addPOIItem(customMarker);
+
+
 
     //xml에 선언된 map_view 레이아웃을 찾아온 후, 생성한 MapView객체 추가
     RelativeLayout container = (RelativeLayout) findViewById(R.id.map_layout);
--- app/src/main/java/com/apptive/joDuo/isthere/SearchCategory.java
+++ app/src/main/java/com/apptive/joDuo/isthere/SearchCategory.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by joseong-yun on 2017. 4. 24..
  */
 
 public class SearchCategory extends AppCompatActivity {
+  private Spinner category1, category2;
+  private Button btnSubmit;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,38 @@ public class SearchCategory extends AppCompatActivity {
         finish();
       }
     });
+//
+//    addItemsOncategory2();
+//    addListenerOnButton();
+//    addListenerOnSpinnerItemSelection();
+  }
 
+  // add items into spinner dynamically
+  public void addItemsOncategory2() {
+
+    category2 = (Spinner) findViewById(R.id.category2);
+    List<String> list = new ArrayList<String>();
+    list.add("list 1");
+    list.add("list 2");
+    list.add("list 3");
+    ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_spinner_item, list);
+    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    category2.setAdapter(dataAdapter);
+  }
+
+  public void addListenerOnSpinnerItemSelection() {
+    category1 = (Spinner) findViewById(R.id.category1);
+    category1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+  }
+
+  // get the selected dropdown list value
+  public void addListenerOnButton() {
+
+    category1 = (Spinner) findViewById(R.id.category1);
+    category2 = (Spinner) findViewById(R.id.category2);
 
   }
+
+
 }
