826871df88895f4aaa2f16da99e2eee112505776
extJo <josungyun6518@gmail.com>
2017-05-24 16:45:40 +0900

    로그인 dialog 완성

--- app/src/main/java/com/apptive/joDuo/isthere/Setting.java
+++ app/src/main/java/com/apptive/joDuo/isthere/Setting.java
@@ -38,6 +38,8 @@ public class Setting extends AppCompatActivity {
         setContentView(R.layout.setting);
 
         Login = (TextView) findViewById(R.id.login);
+        Individual = (TextView) findViewById(R.id.individual);
+        Clause = (TextView) findViewById(R.id.clause);
         Error = (TextView) findViewById(R.id.errorCall);
         Version = (TextView) findViewById(R.id.version);
 
@@ -50,6 +52,12 @@ public class Setting extends AppCompatActivity {
                 loginPage.show();
             }
         });
+
+        // 개인정보 처리방침
+
+
+        // 이용약관
+
 
         // Error feedback
         Error.setOnClickListener(new View.OnClickListener() {
