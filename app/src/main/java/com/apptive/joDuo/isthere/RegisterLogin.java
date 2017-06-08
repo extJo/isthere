44e03b5158e4b4dce58edfd93a1e311464753620
youngho-jo <zeroho.jo@gmail.com>
2017-06-08 20:07:12 +0900

    회원가입 뷰와 httphepler 연결

--- app/src/main/java/com/apptive/joDuo/isthere/RegisterLogin.java
+++ app/src/main/java/com/apptive/joDuo/isthere/RegisterLogin.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
